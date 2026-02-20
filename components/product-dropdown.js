fb349082c422df8a0c9fd3f0a305684ad2f5c490
imrofayel <adamrofayel@gmail.com>
2026-02-20 12:47:52 +0500

    feat: enhance product dropdown styling and make table header sticky

--- components/product-dropdown.js
+++ components/product-dropdown.js
@@ -6,7 +6,7 @@ class ProductDropdown extends HTMLElement {
     this.innerHTML = `
       <ot-dropdown>
         <div class="flex items-start gap-x-2">
-          <button popovertarget="${id}" class="button-white w-full flex items-center justify-between">
+          <button popovertarget="${id}" class="button-white w-full flex items-center justify-between" style="anchor-name: --btn-${id}">
             ${label}
             <svg width="24.5" height="24.5" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="1.7">
@@ -17,8 +17,8 @@ class ProductDropdown extends HTMLElement {
           <button class="simple" title="Select the capacity of the device. A Capacity class combines similar capacities, e.g., 256 GB, 240 GB, 200 GB"><svg xmlns="http://www.w3.org/2000/svg" width="32.5" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linejoin="round"><circle cx="12" cy="12" r="9" stroke-linecap="round" stroke-width="1.5"/><path stroke-width="2.25" d="M12 8h.01v.01H12z"/><path stroke-linecap="round" stroke-width="1.5" d="M12 12v4"/></g></svg></button>
         </div>
 
-        <menu popover id="${id}">
-          <div class="card bg-gray-100 flex flex-col gap-3 w-full text-[18px] dropdown-content">
+        <menu popover id="${id}" style="position-anchor: --btn-${id}; width: anchor-size(width)">
+          <div class="card bg-gray-100 flex flex-col gap-3 text-[18px] dropdown-content" >
           </div>
         </menu>
       </ot-dropdown>
